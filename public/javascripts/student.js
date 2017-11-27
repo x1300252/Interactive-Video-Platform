06885d209dc4b9c2aec2e6086ee43baa75827e27
x1300252 <x1300252@gmail.com>
2017-11-27 11:05:19 +0800

    student mode: disable seek and set pause

--- public/javascripts/student.js
+++ public/javascripts/student.js
@@ -1,8 +1,38 @@
-var ques = [];
-
 $(document).ready(function() {
+    var ques = [];
     $.getJSON("/student/getList", function(data) {
         for (var i = 0; i < data.length; i++)
             ques.push(data[i]);
     });
+
+    console.log(ques);
+
+    //disable jwplayer seek function
+    var maxPlayPosition = 0;
+    var seeking = false;
+    playerInstance.onTime(function (event) {
+        if (!seeking) 
+            maxPlayPosition = Math.max(event.position, maxPlayPosition)
+    }).onPlaylistItem(function () {
+        maxPlayPosition = 0
+    }).onSeek(function (event) {
+        if (!seeking) {
+            if (event.offset > maxPlayPosition) {
+                seeking = true;
+                setTimeout(function () {
+                    playerInstance.seek(maxPlayPosition)
+                }, 100)
+            }
+        } else seeking = false
+    });
+
+    var quesptr=0;
+    playerInstance.on('time', function(x){
+        if(parseInt(x.position,10)==ques[quesptr].time && quesptr < ques.length) {
+                playerInstance.pause(true);
+                playerInstance.setControls(false);
+                quesptr++;
+        }
+    });
 });
+
